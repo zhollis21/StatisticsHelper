70a32f536a1971716812c22f3388af5339654599
Zach Hollis <43797367+zhollis21@users.noreply.github.com>
2020-12-22 23:53:28 -0500

    Program Basics

---
+++ Chapter1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticsHelper
+{
+    public static class Chapter1
+    {
+        public static Random RandomGenerator = new Random();
+
+        public static void CalculatePercentage()
+        {
+            Console.Write("TODO");
+        }
+
+        public static void SumNumbers()
+        {
+            double sum = 0;
+            Console.WriteLine("Please enter the numbers seperated by a single space");
+
+            var input = Console.ReadLine();
+
+            var splitInput = input?.Split(' ');
+
+            foreach (string numberToParse in splitInput)
+            {
+                if (double.TryParse(numberToParse, out double number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine($"\tUnable to parse '{numberToParse}', skipping this item");
+                }
+            }
+
+            Console.WriteLine($"\n\tSum: {sum}");
+        }
+
+        public static void GetRandomNumber()
+        {
+            Console.Write("Please enter a lower bound (default is 0): ");
+            int.TryParse(Console.ReadLine(), out int lowerBound);
+
+            Console.Write($"Please enter a upper bound (default is {int.MaxValue-1}): ");
+            if(!int.TryParse(Console.ReadLine(), out int upperBound))
+            {
+                upperBound = int.MaxValue;
+            }
+            else
+            {
+                upperBound++; // This is to make the upper bound inclusive instead of exclusive
+            }
+
+            Console.WriteLine($"\n\tRandom Number: {RandomGenerator.Next(lowerBound, upperBound)}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace StatisticsHelper
+{
+    public class Program
+    {
+        public enum MenuAction { PercentageCalculator, Quit, GetRandomNumber, SumNumbers }
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello, welcome to statistics helper...\n\tBy Zach & Paula Hollis");
+
+            while (true)
+            {
+                PrintMenu();
+
+                var action = GetMenuAction();
+                if (action == MenuAction.Quit)
+                {
+                    return;
+                }
+
+                CallAction(action);
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine(
+                "\nPlease type the corresponding letter to select a helper..." +
+                "\n\tP: Percentage Calculator" +
+                "\n\tR: Random Number" +
+                "\n\tS: Sum Numbers" +
+                "\n\n\tQ: Quit the Program"
+                );
+        }
+
+        private static MenuAction GetMenuAction()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+
+                char command = char.ToUpper(input[0]);
+
+                if (command == 'P')
+                    return MenuAction.PercentageCalculator;
+                else if (command == 'Q')
+                    return MenuAction.Quit;
+                else if (command == 'R')
+                    return MenuAction.GetRandomNumber;
+                else if (command == 'S')
+                    return MenuAction.SumNumbers;
+            }
+        }
+
+        private static void CallAction(MenuAction action)
+        {
+            if (action == MenuAction.PercentageCalculator)
+            {
+                Chapter1.CalculatePercentage();
+            }
+            else if (action == MenuAction.GetRandomNumber)
+            {
+                Chapter1.GetRandomNumber();
+            }
+            else if (action == MenuAction.SumNumbers)
+            {
+                Chapter1.SumNumbers();
+            }
+        }
+    }
+}
